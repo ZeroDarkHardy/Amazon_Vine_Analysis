dd15fa28132e67116ae646d0510dd85c0c39027e
Matt Hardy <corporalhardy@gmail.com>
2022-05-03 16:11:31 -0700

    added downloads of colab notebooks, schema.sql

--- practice/nlp_practice.py
+++ practice/nlp_practice.py
@@ -1,5 +1,5 @@
 import nltk
 from nltk import word_tokenize
-text = word_tokenize("I enjoy biking on the trails")
+text = word_tokenize("I'm trying to enter a new professional field.")
 output = nltk.pos_tag(text)
 print(output)
